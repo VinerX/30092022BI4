7b0be5535fd7f541b3a32e654f9d669619e51518
VinerX <60321176+VinerX@users.noreply.github.com>
2022-10-05 21:29:02 +0300

    New updates

--- 1.py
+++ 1.py
@@ -2,7 +2,7 @@ Name = input("Введите Имя: ")
 SurName = input("Введите Фамилию: ")
 Date = int(input("Введите год рождения: "))
 
-print(Name,SurName,Date,sep="_")
+print(Name, SurName ,Date, sep="_")
 Name, SurName = SurName,Name
 Date+=60
 print(Name,SurName,Date,sep="_")
