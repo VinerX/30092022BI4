3649e6d562b6fd2280646897e2f789e79a3ab3f2
VinerX <60321176+VinerX@users.noreply.github.com>
2022-10-05 21:18:19 +0300

    First modificate

---
+++ 1.py
@@ -0,0 +1,8 @@
+Name = input("Введите Имя: ")
+SurName = input("Введите Фамилию: ")
+Date = int(input("Введите год рождения: "))
+
+print(Name,SurName,Date,sep="_")
+Name, SurName = SurName,Name
+Date+=60
+print(Name,SurName,Date,sep="_")
---
+++ 2.py
@@ -0,0 +1,7 @@
+Katet1=float(input("Введите длину первого катета: "))
+Katet2=float(input("Введите длину второго катета: "))
+S=Katet1*Katet2
+Gipotenuza=(Katet1**2+Katet2**2)**0.5
+P=Katet1+Katet2+Gipotenuza
+print("Площадь равна: ", S)
+print("Периметр равен: ", P)
